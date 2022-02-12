d0555def9d506a1e8215ec22efe4fef35243fbe7
Shreya <ss2748@cam.ac.uk>
2022-02-12 13:32:50 +0000

    state

--- Functions.py
+++ Functions.py
@@ -18,3 +18,17 @@ def cost_function(time,power,start,end):
 def read_example_csv():
     df = pd.read_csv('example_data.csv', sep=' ', index_col=0, parse_dates=True)
     return df
+
+
+ex_threshold_dictionary = {"off" : [0,200], "idle" : [201,500], "on": [501]}
+def state(current_power, threshold_dictionary):
+  off_upper_limit = threshold_dictionary.get("off")[1]
+  idle_lower_limit = off_upper_limit + 1
+  idle_upper_limit = threshold_dictionary.get("idle")[1]
+  on_lower_limit = idle_upper_limit + 1
+  if (current_power >= on_lower_limit):
+    return "On"
+  elif (current_power >= 201):
+    return "Idle"
+  else:
+    return "Off"
