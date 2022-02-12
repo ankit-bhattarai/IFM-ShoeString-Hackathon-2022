8d6f5697d78de095e7ecdcfbb31b5afee3ce652f
Eduard Burlacu <efb48@cam.ac.uk>
2022-02-12 13:29:45 +0000

    read_example_csv()

--- Functions.py
+++ Functions.py
@@ -1,2 +1,20 @@
 #Here add all required functions.
-def cost_function():
+from _datetime import datetime
+
+import pandas as pd
+
+
+def cost_function(time,power,start,end):
+    ''
+    energy=0
+    cost=0
+    dt=(time[-1]-time[0])/len(time)
+    newtime=time.copy()
+    for timp in newtime:
+        date_time_object=datetime.strftime(timp,'%H:%M:%S')
+    for i in range(1,len(time)):
+        energy+=power[i]*dt
+
+def read_example_csv():
+    df = pd.read_csv('example_data.csv', sep=' ', index_col=0, parse_dates=True)
+    return df
--- data_collection.py
+++ data_collection.py
@@ -3,6 +3,6 @@ import numpy as np
 #Open file
 my_file=pd.read_csv('example_data.txt', sep=" ")
 file=pd.DataFrame(my_file)
-
+print(file)
 #Now I am creating a new csv with data to be fetched into the table.
 #table=pd.open_csv('table')
